42c4a03a2dd051d016c4232a55592f1f3e900b8c
Emanuel Oliveira <emanueloliveira38@yahoo.com.br>
2023-10-25 20:09:56 -0300

    feat: implement front requests

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import axios from 'axios'
 import { BrowserRouter as Router } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [fibonacci, setFibonacci] = useState("Carregando");
+  const [factorial, setFactorial] = useState("Carregando");
+
+  const api = axios.create({
+    baseURL: process.env.API_URL || 'http://localhost:8080/api',
+  });
+
   useEffect(() => {
-    fetch();
+    const number = new URLSearchParams(document.location.search).get('number');
+    api.get(`/fibonacci?number=${number}`, {}).then((res) => {
+      setFibonacci(res.data.message);
+    }).catch((res) => {
+      setFibonacci(res.data.message)
+    });
+    api.get(`/factorial?number=${number}`, {}).then((res) => {
+      setFactorial(res.data.message);
+    }).catch((res) => {
+      setFactorial(res.data.message)
+    });
   }, []);
 
   return (
     <Router basename="/app">
-      <div>Teste</div>
+      <div>
+        <div>Factorial: {factorial}</div>
+        <div>Fibonacci: {fibonacci}</div>
+      </div>
     </Router>
   );
 }
